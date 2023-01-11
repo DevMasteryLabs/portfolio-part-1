9e585444bc92bab0494a90a6285096804a5dd963
DevMasteryLabs <develop.devmastery@gmail.com>
2023-01-11 22:53:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container'>
+          <Route exact path='/' component={Home} />
+          <Route path='/skills' component={Skills} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/contact' component={Contact} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
